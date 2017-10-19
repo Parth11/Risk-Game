324f54492c8aeb26d10dbaffc27bde87cef90ed6
Shivangi-PC\Shivangisingh06 <shivangisingh06@gmail.com>
2017-10-18 22:27:49 -0400

    test commit

--- src/ca/concordia/app/controller/MainController.java
+++ src/ca/concordia/app/controller/MainController.java
@@ -10,20 +10,31 @@ import java.awt.event.MouseListener;
 
 import ca.concordia.app.view.MainView;
 
+// TODO: Auto-generated Javadoc
 /**
- * @author harvi
+ * The Class MainController.
  *
+ * @author harvi
  */
 public class MainController implements ActionListener, MouseListener {
 	
 	
+	/** The main view. */
 	public MainView main_view;
 	
 	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args){
 		new MainController();
 	}
 	
+	/**
+	 * Instantiates a new main controller.
+	 */
 	public MainController() {
 		
 		main_view = new MainView();
@@ -32,36 +43,54 @@ public class MainController implements ActionListener, MouseListener {
 		main_view.setVisible(true);
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseClicked(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mousePressed(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseEntered(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseExited(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println(e);
