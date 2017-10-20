2d0b84ea39308e05e520baf4879beb3b546c68a2
AbhinavSingh009 <abhinav.singh1009@gmail.com>
2017-10-20 13:52:22 -0400

    Modifications to the Continent Class Added JavaDoc

--- src/ca/concordia/app/model/Continent.java
+++ src/ca/concordia/app/model/Continent.java
@@ -8,42 +8,84 @@ import java.awt.Color;
  *
  */
 public class Continent {
-
+	
+	/**
+	 * continent name of type String.
+	 */
 	private String continent_name;
 	
+	/**
+	 * The control value of the continent of type int.
+	 */
 	private int control_value;
 	
+	/**
+	 * The color of the continent of type Color
+	 */
 	private Color colour;
 
+	/**
+	 * A parameterized constructor.
+	 * @param continentName
+	 * @param controlValue
+	 * @param colour
+	 */
 	public Continent(String continentName, int controlValue, Color colour) {
 		this.continent_name = continentName;
 		this.control_value = controlValue;
 		this.colour = colour;
 	}
 
+	/**
+	 * 
+	 * @return the continent name.
+	 */
 	public String getContinentName() {
 		return continent_name;
 	}
-
+	
+	/**
+	 * Sets the continent name.
+	 * @param continentName
+	 */
 	public void setContinentName(String continentName) {
 		this.continent_name = continentName;
 	}
 
+	/**
+	 * 
+	 * @return the control value of the continent.
+	 */
 	public int getControlValue() {
 		return control_value;
 	}
 
+	/**
+	 * Sets the control value of the continent.
+	 * @param controlValue
+	 */
 	public void setControlValue(int controlValue) {
 		this.control_value = controlValue;
 	}
+	/**
+	 * 
+	 * @return the color which will be used for defining the continent. 
+	 */
 	public Color getColor() {
 		return colour;
 	}
-
+	
+	/**
+	 * sets the color to the continent.
+	 * @param color
+	 */
 	public void setColor(Color color) {
 		this.colour = color;
 	}
 
+	/**
+	 * @return boolean value True if the two continents are equal.
+	 */
 	public boolean equals(Object obj) {
 		  
 		  if(obj instanceof Continent){
