8177b03c78ef2c284a39a9f17359729fe4cf2195
Shivangisingh06 <shivangisingh06@gmail.com>
2017-10-20 14:09:30 -0400

    javadoc to the GameContants

--- src/ca/concordia/app/util/GameConstants.java
+++ src/ca/concordia/app/util/GameConstants.java
@@ -1,19 +1,25 @@
 package ca.concordia.app.util;
+
 /**
- * 
- * @author harvi
+ * The Interface GameConstants.
  *
+ * @author harvi
  */
 public interface GameConstants {
 
+	/** The Constant NEW_LINE. */
 	public static final String NEW_LINE = "\n";
 	
+	/** The Constant MAP_HEADER_CONST. */
 	public static final String MAP_HEADER_CONST = "[Map]";
 	
+	/** The Constant MAP_AUTHOR. */
 	public static final String MAP_AUTHOR = "Team 6";
 	
+	/** The Constant CONTINENT_HEADER_CONST. */
 	public static final String CONTINENT_HEADER_CONST = "[Continents]";
 	
+	/** The Constant TERRITORY_HEADER_CONST. */
 	public static final String TERRITORY_HEADER_CONST = "[Territories]";
 	
 	
