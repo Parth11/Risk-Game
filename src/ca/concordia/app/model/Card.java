4e3ae653bdc66d232c9644531c52345d0e974d4e
AbhinavSingh009 <abhinav.singh1009@gmail.com>
2017-10-20 13:35:25 -0400

    Javadoc added to Cards.

--- src/ca/concordia/app/model/Card.java
+++ src/ca/concordia/app/model/Card.java
@@ -9,13 +9,24 @@ public class Card {
 
 	enum CardType {INFANTRY, CAVALRY, ARTILLERY};
 	
+	/**
+	 * Create an object of class CardType.
+	 */
 	private CardType card_type;
-
+	
+	/**
+	 * Sets the card type
+	 * @param cardType
+	 */
 	public Card(CardType cardType) {
 		super();
 		this.card_type = cardType;
 	}
-
+	
+	/**
+	 * 
+	 * @return the card type.
+	 */
 	public CardType getCardType() {
 		return card_type;
 	}
