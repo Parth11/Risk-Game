7f8b9a94c1e390ef8fcf048d40d0919c3059effc
Shivangisingh06 <shivangisingh06@gmail.com>
2017-10-20 14:16:36 -0400

    JavaDoc added to RiskExceptionHandler

--- src/ca/concordia/app/util/RiskExceptionHandler.java
+++ src/ca/concordia/app/util/RiskExceptionHandler.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package ca.concordia.app.util;
 
 import java.awt.Component;
@@ -9,19 +7,30 @@ import java.lang.Thread.UncaughtExceptionHandler;
 import javax.swing.JOptionPane;
 
 /**
- * @author harvi
+ * The Class RiskExceptionHandler.
  *
+ * @author harvi
  */
 public class RiskExceptionHandler implements UncaughtExceptionHandler {
 
+	/** The parent component. */
 	private Component parent_component;
 	
+	/**
+	 * calls the uncaughtException java method
+	 * @param t, e
+	 */
 	@Override
 	public void uncaughtException(Thread t, Throwable e) {
 		e.printStackTrace();
 		JOptionPane.showMessageDialog(parent_component, "Something Went Wrong","Error",JOptionPane.ERROR_MESSAGE);
 	}
 	
+	/**
+	 * Instantiates a new risk exception handler.
+	 *
+	 * @param parentComponent the parent component
+	 */
 	public RiskExceptionHandler(Component parentComponent) {
 		this.parent_component = parentComponent;
 	}
