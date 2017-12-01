50f6d33616cc4d489ff1f457b6166f700463732e
DESKTOP-B9BR0V1\hgagneja <h_gagnej@encs.concordia.ca>
2017-12-01 07:37:49 -0500

    merge fix

--- src/ca/concordia/app/strategies/AggressiveStrategy.java
+++ src/ca/concordia/app/strategies/AggressiveStrategy.java
@@ -62,7 +62,7 @@ public class AggressiveStrategy implements PlayerStrategy {
 		Country strongestCountry = GamePlayService.getInstance().getStrongestCountry(p);
 
 		if (strongestCountry != null) {
-			if (GamePlayService.getInstance().getEligibleAttackableCountries(strongestCountry).isEmpty()) {
+			if (!GamePlayService.getInstance().getEligibleAttackableCountries(strongestCountry).isEmpty()) {
 
 				Country defendingCountry = GamePlayService.getInstance()
 						.getEligibleAttackableCountries(strongestCountry).get(0);
